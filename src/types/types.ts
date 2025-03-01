9742699af66f1afdc0e3c55ddf595f54f713bc28
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-03-01 15:41:28 +0100

    feat(types): add new types for staff and related fields
    - Added `DepartmentType` to represent department options.
    - Added `StaffType` to distinguish between academic and non-academic staff.
    - Added `NotificationType` to specify notification preferences.
    - Added `Staff` interface to define staff member properties.
    - Added `StaffFormValues` type using `Omit` to exclude non-form fields like `id`, `created_at`, and `updated_at`.

--- src/types/types.ts
+++ src/types/types.ts
@@ -13,3 +13,37 @@ export interface User {
   email: string;
   is_admin: boolean;
 }
+
+export type DepartmentType =
+  | "botany"
+  | "computer_science"
+  | "chemistry"
+  | "cell_biology_and_genetics"
+  | "marine_sciences"
+  | "mathematics"
+  | "microbiology"
+  | "physics"
+  | "statistics"
+  | "zoology";
+
+export type StaffType = "academic" | "non_academic";
+
+export type NotificationType = "email" | "phone_number";
+
+export interface Staff {
+  id?: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone_number: string;
+  department?: DepartmentType;
+  staff_type?: StaffType;
+  date_of_birth: string;
+  profile_image_url?: string;
+  notification_type?: NotificationType;
+  is_enabled?: boolean;
+  created_at?: string;
+  updated_at?: string;
+}
+
+export type StaffFormValues = Omit<Staff, "id" | "created_at" | "updated_at">;
